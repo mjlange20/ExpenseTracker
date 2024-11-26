123140cd17e08f6bee8da6e8761ca7e0c8a150f9
Mark Lange <Mark.Lange@ips.corp>
2024-11-26 17:36:17 +0200

    added extra validation for supabase instance in session state

--- Database.py
+++ Database.py
@@ -8,6 +8,9 @@ key = st.secrets['db_key']
 st.session_state['Client'] = create_client(url, key)
 
 def fetch_data():
+    if 'Client' not in st.session_state:
+        st.session_state['Client'] = create_client(url, key)
+    
     data_response = st.session_state['Client'].table("D_Detail").select("*").order("created_at", desc=True).execute()
 
     df_response = pd.DataFrame(data_response.data, columns = ['id','created_at','created_by','type', 'amount', 'description', 'expense_date']) 
@@ -17,6 +20,9 @@ def fetch_data():
     return (df_response)
 
 def add_entry(user, amount, type, description, expense_date):
+    if 'Client' not in st.session_state:
+        st.session_state['Client'] = create_client(url, key)
+        
     response = (
     st.session_state['Client'].table("D_Detail")
     .insert({"created_by": user, "type": type, "amount": amount, "description": description, "expense_date": expense_date})
